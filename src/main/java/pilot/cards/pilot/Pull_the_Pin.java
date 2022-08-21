caa86a68c1be6259017056143af83b3fe9fb671f
CardboardBones <micah.t.design@gmail.com>
2022-08-20 18:36:41 -0700

    Just seeing if deleting and readding fixes my bug

---
+++ src/main/java/pilot/cards/pilot/Pull_the_Pin.java
@@ -0,0 +1,39 @@
+package pilot.cards.pilot;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import pilot.PilotMod;
+import pilot.cards.CustomPilotModCard;
+import pilot.cards.pilot.Strike;
+import pilot.characters.Pilot;
+
+public class Pull_the_Pin extends CustomPilotModCard {
+    public static final String ID = PilotMod.makeID(Pull_the_Pin.class);
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Pilot.Enums.PILOT_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_PLUS_COST = -1;
+
+    public Pull_the_Pin() {
+        super(ID, COST, TYPE, COLOR, RARITY, TARGET);
+        this.cardsToPreview = new Strike();
+    }
+
+    @Override
+    public void use (AbstractPlayer p, AbstractMonster m){
+        addToBot(new MakeTempCardInDrawPileAction(new Strike(), 1, false, true));
+    }
+
+    @Override
+    public void upgrade(){
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_PLUS_COST);
+        }
+    }
+}
