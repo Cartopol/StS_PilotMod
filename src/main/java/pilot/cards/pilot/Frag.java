662409cb5093878f15c9677552c9823a2d397c8e
Cartopol <onmogelijk@gmail.com>
2022-08-22 22:54:01 +0200

    Fixed Frag targeting, and upped it's damage a bit

--- src/main/java/pilot/cards/pilot/Frag.java
+++ src/main/java/pilot/cards/pilot/Frag.java
@@ -12,12 +12,12 @@ public class Frag extends CustomPilotModCard {
     public static final String ID = PilotMod.makeID(Frag.class);
 
     private static final CardRarity RARITY = CardRarity.SPECIAL;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Pilot.Enums.PILOT_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 13;
+    private static final int DAMAGE = 20;
     private static final int UPGRADE_PLUS_DMG = 10;
 
     public Frag() {
